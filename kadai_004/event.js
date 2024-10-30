7383fedfb91fdd80da243021f39074c8f46b503f
Yokereba <kozottio1007@gmail.com>
2024-10-30 09:43:41 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(document).on('load scroll',(e) => {
+        if(e.type === 'load'){
+            console.log('loadイベントが発生しました');
+        }
+        if(e.type === 'scroll'){
+            console.log('scrollイベントが発生しました');
+        }
+    
+    
+     });
+});
+   
